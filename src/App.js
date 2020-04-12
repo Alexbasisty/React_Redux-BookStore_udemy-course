884405de82cb2fe879f89bc529f5e6fbd16523d7
Alex Basystyi <alexbasisty@gmail.com>
2020-04-12 12:28:22 +0200

    prettier config

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 const App = () => {
-    return React.createElement(
-        "div",
+  return React.createElement(
+    "div",
+    {
+      id: "created-by-react",
+    },
+    [
+      React.createElement("h1", {}, "React BoookStore"),
+      React.createElement(
+        Book,
         {
-            id: "created-by-react"
+          title: "Straż! Straż",
+          author: "Terry Prachett",
+          publicationDate: "1989",
         },
-        [
-            React.createElement('h1', {}, "React BoookStore"),
-            React.createElement(Book, {
-                title: "Straż! Straż",
-                author: "Terry Prachett",
-                publicationDate: "1989"
-            }
-            , {title: "", }),
-            React.createElement(Book, {
-                title: "Dwie wieże", 
-                author: "J. R. R. Tolkien", 
-                publicationDate: "1954"
-            }
-            ),
-            React.createElement(Book, {
-                title: "Komnata Tajemnic", 
-                author: "J. K. Rowling", 
-                publicationDate: "1998"
-            }
-            )
-        ]
-    )
-}        
+        { title: "" }
+      ),
+      React.createElement(Book, {
+        title: "Dwie wieże",
+        author: "J. R. R. Tolkien",
+        publicationDate: "1954",
+      }),
+      React.createElement(Book, {
+        title: "Komnata Tajemnic",
+        author: "J. K. Rowling",
+        publicationDate: "1998",
+      }),
+    ]
+  );
+};
 
-const Book = ({title, author, publicationDate}) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h2", {}, title),
-            React.createElement("h3", {}, author),
-            React.createElement("h3", {}, publicationDate),
-        ]
-    )
-}
+const Book = ({ title, author, publicationDate }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, title),
+    React.createElement("h3", {}, author),
+    React.createElement("h3", {}, publicationDate),
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
