c33b0bb31c34608edda357029d2d9cbd3cd80fe2
Alex Basystyi <alexbasisty@gmail.com>
2020-04-12 18:01:51 +0200

    next inputs

--- src/Add.jsx
+++ src/Add.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 
 export const Add = () => {
   const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [audience, setAudience] = useState("");
+
+  const genres = ["Fantastyka", "Horror", "Kryminał"];
+  const audiences = ["Dzieci", "Młodież", "Dorośli"];
 
   return (
     <div>
@@ -16,6 +21,40 @@ export const Add = () => {
               setAuthor(event.target.value);
             }}
           />
+        </label>
+        <br />
+        <label htmlFor="genre">
+          Gatunek
+          <select
+            id="genre"
+            value={genre}
+            onBlur={(e) => setGenre(e.target.value)}
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option>Dowolna</option>
+            {genres.map((genre, index) => (
+              <option key={index} value={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label htmlFor="audience">
+          Dla
+          <select
+            id="audience"
+            value={audience}
+            onBlur={(e) => setAudience(e.target.value)}
+            onChange={(e) => setAudience(e.target.value)}
+          >
+            <option>Dowolna</option>
+            {audiences.map((audience, index) => (
+              <option key={index} value={audience}>
+                {audience}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Dodaj</button>
       </form>
--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,22 @@ const App = () => {
         title="Straż! Straż!"
         author="Terry Prachett"
         publicationDate="1989"
+        genre="Fantastyka"
+        audience="Młodież"
       />
       <Book
         title="Dwie wieże"
         author="J. R. R. Tolkien"
         publicationDate="1954"
+        genre="Fantastyka"
+        audience="Młodież"
       />
       <Book
         title="Komnata Tajemnic"
         author="J. K. Rowling"
         publicationDate="1998"
+        genre="Fantastyka"
+        audience="Młodież"
       />
     </div>
   );
